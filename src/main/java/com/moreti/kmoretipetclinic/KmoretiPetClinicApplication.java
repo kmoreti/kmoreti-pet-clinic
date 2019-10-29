d0d13f100ad3d8c8e61a3448666a98f24ba6ae88
Kleber Moreti de Camargo <kleber.camargo@fatec.sp.gov.br>
2019-10-29 22:11:15 +0000

    initial commit

---
+++ src/main/java/com/moreti/kmoretipetclinic/KmoretiPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.moreti.kmoretipetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmoretiPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmoretiPetClinicApplication.class, args);
+	}
+
+}
