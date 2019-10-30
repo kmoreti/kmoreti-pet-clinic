c19943220c9fee792691054f35a4cbbbd2c19ad1
Kleber Moreti de Camargo <kleber.camargo@fatec.sp.gov.br>
2019-10-30 20:46:44 +0000

    Implement Map Based Services

---
+++ pet-clinic-data/src/main/java/com/moreti/kmoretipetclinic/service/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package com.moreti.kmoretipetclinic.service.map;
+
+import com.moreti.kmoretipetclinic.model.Vet;
+import com.moreti.kmoretipetclinic.service.VetService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        super.delete(vet);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return super.save(vet.getId(), vet);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
