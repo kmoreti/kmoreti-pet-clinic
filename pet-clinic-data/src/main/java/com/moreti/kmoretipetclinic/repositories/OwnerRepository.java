d60fe34cb27f8790a6ecbe043b30ce44b4155859
Kleber Moreti de Camargo <kleber.camargo@fatec.sp.gov.br>
2019-11-09 11:16:43 +0000

    Added spring data jpa service for owners

--- pet-clinic-data/src/main/java/com/moreti/kmoretipetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/moreti/kmoretipetclinic/repositories/OwnerRepository.java
@@ -4,4 +4,6 @@ import com.moreti.kmoretipetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner finByLastName(String lastName);
 }
