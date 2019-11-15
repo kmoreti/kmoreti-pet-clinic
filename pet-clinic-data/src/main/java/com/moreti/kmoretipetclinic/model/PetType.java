60cc417756c5f3c269ba8350dbc09ca6198ba06e
Kleber Moreti de Camargo <kleber.camargo@fatec.sp.gov.br>
2019-11-15 22:05:08 +0000

    Added builder pattern to Pet Type

--- pet-clinic-data/src/main/java/com/moreti/kmoretipetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/moreti/kmoretipetclinic/model/PetType.java
@@ -18,4 +18,14 @@ public class PetType extends BaseEntity {
     @Column(name = "name")
     private String name;
 
+    @Builder
+    public PetType(Long id, String name) {
+        super(id);
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
